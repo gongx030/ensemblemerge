445e7e2f09cd60da6089ada32be73a54f2ecd130
erikjskie <erikjskie@users.noreply.github.com>
2022-01-07 08:11:09 -0600

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -19,5 +19,6 @@ install_python_packages <- function(){
 .onLoad <- function(libname, pkgname) {
   install_python_packages()
   devtools::install_github("cellgeni/sceasy")
+  install.packages("rsvd")
   devtools::install_github('satijalab/seurat-wrappers')
 }
