d9880e23731254040be05e6cb765d5fa93e8ef66
erikjskie <erikjskie@users.noreply.github.com>
2022-01-10 10:52:41 -0600

    Update onload

--- R/zzz.R
+++ R/zzz.R
@@ -1,18 +1,5 @@
 #' on load initialization of reticulate environment
 
-#' @export
-install_python_packages <- function(){
-  reticulate::py_install("scipy")
-  reticulate::py_install("pandas")
-  reticulate::py_install("numpy")
-  reticulate::py_install("scanpy")
-  system("pip install scanorama")
-  system("pip install anndata")
-  system("pip install bbknn")
-  system("pip install scipy")
-}
-
 .onLoad <- function(libname, pkgname) {
   devtools::install_github("cellgeni/sceasy")
-  #devtools::install_github('satijalab/seurat-wrappers')
 }
