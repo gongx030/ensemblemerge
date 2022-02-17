60d4929d5f6681c80f4b16baf4baabb3296ef35a
Wuming Gong <gongx030@umn.edu>
2022-02-17 13:24:33 -0600

    update

--- R/runSeurat.R
+++ R/runSeurat.R
@@ -1,5 +1,7 @@
 #' Run Seurat functions
 #'
+#' Adopted from https://satijalab.org/seurat/articles/integration_introduction.html
+#'
 #' @param params a SeuratParam object
 #' @param data a Seurat object
 #'
@@ -10,24 +12,46 @@
 #'
 run_Seurat <- function(params, data){
 
+	features <- VariableFeatures(data)
+
+	d <- SplitObject(data, split.by = params@batch)
+
 	# when k.weight is greater than number of cell in any batches, it will result such error:
 	# IntegrateData error: Error in idx[i, ] <- res[[i]][[1]]
 	# https://github.com/satijalab/seurat/issues/3930
-	n <- sapply(data, ncol) # number of cells per cluster
-	params@k.weight <- min(n)
+	#
+	n <- sapply(d, ncol) # number of cells per cluster
+	params@k.weight <- min(n, params@k.weight)
 	sprintf('run_Seurat | setting params@k.weight to %d', params@k.weight) %>% message()
 
-  cell_anchors <- FindIntegrationAnchors(object.list = data, dims = 1:params@npcs)
- 	data <- IntegrateData(anchorset = cell_anchors, dims = 1:params@npcs, k.weight = params@k.weight)
+  cell_anchors <- FindIntegrationAnchors(
+		object.list = d, 
+		dims = 1:params@npcs, 
+		anchor.features = features
+	)
+
+ 	d <- IntegrateData(
+		anchorset = cell_anchors, 
+		dims = 1:params@npcs, 
+		k.weight = params@k.weight
+	)
 
   if(params@regressUMI && params@scaling) {
-    data <- ScaleData(object = data , vars.to.regress = params@vars.to.regress)
+    d <- ScaleData(object = d, vars.to.regress = params@vars.to.regress)
   } else if(params@scaling) { # default option
-    data <- ScaleData(object = data)
+    d <- ScaleData(object = d)
   }
 
-  data <- RunPCA(data, npcs = params@npcs, reduction.name = params@name)
+  d <- RunPCA(
+		d, 
+		npcs = params@npcs, 
+		reduction.name = params@name,
+		reduction.key = params@reduction_key
+	)
 
+	d@reductions[[params@name]]@assay.used <- data@active.assay
+	data[[params@name]] <- d@reductions[[params@name]]
+		
 	data
 
 }
