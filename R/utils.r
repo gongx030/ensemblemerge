32f0cf16fde405e6905791b5ccacab52daf1d753
Wuming Gong <gongx030@umn.edu>
2022-03-10 10:21:48 -0600

    init

---
+++ R/utils.r
@@ -0,0 +1,16 @@
+.check_method <- function(x){
+	available_methods <- c(
+		"Seurat", 
+		"Scanorama", 
+		"Harmony", 
+		"Liger", 
+		"BBKNN", 
+		"Uncorrected", 
+		"fastMNN", 
+		"scVI"
+	)
+  ### checking valid parameters ###
+  if(!all(x%in% availableMethods)){
+    stop(sprintf("method must be the following: %s", paste(available_methods, collapse = ", ")))
+  }
+}
