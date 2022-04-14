d7722514bc770f87e119b64f5f58eef70ddcd820
Wuming Gong <gongx030@umn.edu>
2022-04-14 17:02:58 -0500

    update

---
+++ R/GeneMarkers.r
@@ -0,0 +1,35 @@
+setClass(
+	'PanglaoDBGeneMarkers',
+	representation(
+		celltype = 'data.frame',
+		genome = 'character',
+		level = 'character'
+	),
+	contains = 'BaseGeneMarkers',
+	prototype(
+		check_dependencies = TRUE,
+		genome = 'hg19',
+		level = 'Level4',
+		dependences = list(
+			new('RPackage', package_name = 'scMRMA', package_version = '1.0')
+		)
+	),
+	validity = function(object){
+		msg <- NULL
+		if (!object@genome %in% c('hg19', 'mm10'))
+			msg <- sprintf('unknown genome: %s', object@genome)
+		return(msg)
+	}
+)
+
+#' @importFrom methods callNextMethod
+#'
+setMethod('initialize', 'PanglaoDBGeneMarkers', function(.Object, check_dependencies = TRUE, ...){
+	.Object <- callNextMethod(.Object, check_dependencies = check_dependencies, ...)
+	if (.Object@genome == 'hg19'){
+		.Object@celltype <- local({get(load(system.file("data", "Human_PanglaoDB.Rdata", package = "scMRMA")))})
+	}else if (.Object@genome == 'mm10'){
+		.Object@celltype <- local({get(load(system.file("data", "Mouse_PanglaoDB.Rdata", package = "scMRMA")))})
+	}
+	.Object
+})
