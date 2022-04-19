5114cc258b396272473d896d70dcb021f63092c8
Wuming Gong <gongx030@umn.edu>
2022-04-19 16:30:05 -0500

    update

--- R/runHarmony.R
+++ R/runHarmony.R
@@ -9,26 +9,6 @@
 #'
 run_Harmony <- function(params, data){
 
-	if (is.null(data@reductions[[params@pca_name]])){
-  	data <- RunPCA(
-			object = data, 
-			npcs = params@npcs, 
-			reduction.name = params@pca_name,
-			verbose = FALSE
-		)
-	}
-
-  data <- harmony::RunHarmony(
-		object = data, 
-		params@batch, 
-		theta = params@theta, 
-		plot_convergence = FALSE, 
-		max.iter.cluster = params@max_iter_cluster,
-		assay.use = params@raw_assay,
-		reduction.save = params@name,
-		verbose = TRUE
-	)
-
 	data
 
 }
