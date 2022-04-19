74f255b6193b215ee49fddea3fbcbf30641ec1ed
Wuming Gong <gongx030@umn.edu>
2022-04-19 10:59:17 -0500

    update

--- R/runSeurat.R
+++ R/runSeurat.R
@@ -12,35 +12,6 @@
 #'
 run_Seurat <- function(params, data){
 
-	# when k.weight is greater than number of cell in any batches, it will result such error:
-	# IntegrateData error: Error in idx[i, ] <- res[[i]][[1]]
-	# https://github.com/satijalab/seurat/issues/3930
-	n <- sapply(data, ncol) # number of cells per cluster
-	params@k.weight <- min(n, params@k.weight)
-	sprintf('run_Seurat | setting params@k.weight to %d', params@k.weight) %>% message()
-
-  data <- FindIntegrationAnchors(
-		object.list = data, 
-		dims = 1:params@npcs,
-		verbose = FALSE
-	)
-
- 	data <- IntegrateData(
-		anchorset = data, 
-		dims = 1:params@npcs, 
-		k.weight = params@k.weight,
-		verbose = FALSE
-	)
-
-  data <- ScaleData(object = data, verbose = FALSE)
-
-  data <- RunPCA(
-		data, 
-		npcs = params@npcs, 
-		reduction.name = params@name,
-		reduction.key = params@reduction_key,
-		verbose = FALSE
-	)
 
 	data
 
