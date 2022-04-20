f7d21caade6226cf1f70888c679eaf1b18c15f88
Wuming Gong <gongx030@umn.edu>
2022-04-20 17:31:01 -0500

    update

--- R/ambient_removal.r
+++ R/ambient_removal.r
@@ -99,3 +99,78 @@ setMethod(
 	}
 )
 
+
+setClass(
+	'soupXRemoveAmbientRNA',
+	representation(
+	),
+	contains = c('BaseAmbientRNARemoval'),
+	prototype(
+		name = 'soupXRemoveAmbientRNA',
+		dependences = list(
+			new('RPackage', package_name = 'SoupX', package_version = '1.5.2')
+		)
+	)
+)
+
+#' Remove ambient RNAs by soupX (https://github.com/constantAmateur/SoupX)
+#'
+#' @param x a Seurat object
+#' @param params a decontXRemoveAmbientRNA object
+#' @param ... Additional arguments
+#' @return returns a data object with ambient RNA removed
+#' @references Matthew D Young, Sam Behjati, SoupX removes ambient RNA contamination from droplet-based single-cell RNA sequencing data, GigaScience, Volume 9, Issue 12, December 2020, giaa151, https://doi.org/10.1093/gigascience/giaa151
+#' @export
+#'
+setMethod(
+	'RemoveAmbientRNA',
+	signature(
+		x = 'Seurat',
+		params = 'soupXRemoveAmbientRNA'
+	),
+	function(
+		x,
+		params,
+		...
+	){
+
+		params_embed <- new('PCAEmbed', normalize = params@normalize)
+		x <- Embed(x, params_embed)
+
+	  params_cluster <- new('LouvainCluster', embedding = params_embed)
+		x <- Cluster(x, params_cluster)
+
+		raw_assay <- params@normalize@assay_name
+		toc <- x@assays[[raw_assay]]@counts
+		sc <- SoupX::SoupChannel(toc, toc, calcSoupProfile = FALSE)
+
+		soupProf <- data.frame(row.names = rownames(toc), est = rowSums(toc)/sum(toc), counts = rowSums(toc))
+		sc <- SoupX::setSoupProfile(sc, soupProf)
+
+		sc <- SoupX::setClusters(sc, x[[params_cluster@cluster_name]][, 1])
+
+		sc <- SoupX::autoEstCont(
+			sc,
+			topMarkers = NULL,
+			tfidfMin = 1,
+			soupQuantile = 0.9,
+			maxMarkers = 100,
+			contaminationRange = c(0.01, 0.8),
+			rhoMaxFDR = 0.2,
+			priorRho = 0.05,
+			priorRhoStdDev = 0.1,
+			doPlot = FALSE,
+			forceAccept = FALSE,
+			verbose = TRUE
+		)
+
+		out <- SoupX::adjustCounts(sc)
+
+		x[[params@normalize@assay_name]] <- CreateAssayObject(
+			counts = out
+		)
+		x <- Normalize(x, params@normalize)
+		x
+	}
+)
+
